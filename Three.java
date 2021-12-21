d4a329e0bb551968ee0022a31d4aca61236c7b94
unknown <maheshvjogi.xworkz@gmail.com git config --global user.email maheshvjogi.xworkz@gmail.com>
2021-12-21 11:17:16 +0530

    lol2

---
+++ Three.java
@@ -0,0 +1,16 @@
+class Three{
+	public static void main(String[] am){
+		
+		long price[] = {1111111,2222222,3333333,4444444,5555555,6666666,7777777,8888888,9999999,10000000};
+		System.out.println(price[0]);
+		System.out.println(price[1]);
+		System.out.println(price[2]);
+		System.out.println(price[3]);
+		System.out.println(price[4]);
+		System.out.println(price[5]);
+		System.out.println(price[6]);
+		System.out.println(price[7]);
+		System.out.println(price[8]);
+		System.out.println(price[9]);
+	}
+}
